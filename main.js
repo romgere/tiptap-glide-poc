28b466a783123504a5583124328d8912be0d4a9d
Jérôme Mestres <jerome@mestres.fr>
2024-03-22 14:47:30 +0100

    fix

--- main.js
+++ main.js
@@ -225,6 +225,8 @@ document.getElementById('wc-btn').addEventListener('click', function () {
 
     dialogOK.innerText = 'Update';
   } else {
+    document.getElementById('foo-input').value = 'Foo value';
+    document.getElementById('bar-input').value = 'Bar value';
     dialogOK.innerText = 'Insert';
   }
   dialog.show();
